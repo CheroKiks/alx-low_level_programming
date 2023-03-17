709382c4393f874c2522fa75f78b1299f75f4e52
CheroKiks <cherotichirine@gmail.com>
2023-03-16 22:32:01 -0700

    correct sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * Return: always 0 (Success)
  */
 int main(void)
@@ -11,4 +12,5 @@ int main(void)
 	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
+
 }
