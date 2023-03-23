d1ed32ce36ecdac7e175d31c87b2c3b3b2d4c485
CheroKiks <cherotichirine@gmail.com>
2023-03-23 01:29:40 -0700

    update times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,24 +7,22 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
+		_putchar('0');
 
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			k = j * i;
+
+			if (k == 9)
 				_putchar(' ');
-				_putchar(' ');
+			else
 				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+			_putchar((k % 10) + '0');
 		}
 		_putchar('\n');
 	}
