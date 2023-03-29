ccc46375f876cd0b22c2dcdcf46ebca4518367eb
CheroKiks <cherotichirine@gmail.com>
2023-03-28 20:00:02 -0700

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,12 +17,10 @@ void puts_half(char *str)
 	}
 	if (count % 2 == 1)
 		i = count / 2;
-	{
 	else
 		i = (count - 1) / 2;
 
 	for (i++; i < count; i++)
 		_putchar(str[i]);
-	}
-	_putchar('\n');
+ _putchar('\n');
 }
