2d7c4001ce94fba22f64bd584b825eb14be49141
CheroKiks <cherotichirine@gmail.com>
2023-03-30 20:56:53 -0700

    3_compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - compares two strings.
  * @s1: pointer to first string.
  * @s2: pointer to second string.
- * Return: value less than 0 if less than the other, value greater than 0 if greater than the other and 0 if equal
+ * Return: value < 0 if < other, value > 0 if > other and 0 if equal
  */
 
 int _strcmp(char *s1, char *s2)
