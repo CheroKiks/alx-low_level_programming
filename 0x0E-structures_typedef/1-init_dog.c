caba57d9657a7a3cfa83eadaf8e4265089da28f4
CheroKiks <cherotichirine@gmail.com>
2023-04-17 19:48:44 -0700

    define new type

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,19 @@
 #include "dog.h"
+
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: ptr to struct dog
- * @name: ptr to char name which is dog's name
- * @age: age of the dog
- * @owner: ptr to char owner of the dog
- * Return: NULL
- */
+* init_dog - initialized a var type of struct dog
+*@d: dog
+*@name: dog name
+*@age: dog age
+*@owner: dog owner
+*/
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
-		d->name = name;
-		d->age = age;
-	       d->owner = owner;
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
 }
